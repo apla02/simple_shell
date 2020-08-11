31e6d5b7ab350df9296dcca94f9d6f77e250d6cd
apla02 <1873@holbertonschool.com>
2020-08-11 11:30:37 -0500

    header

---
+++ holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+char *_which(char *s);
+#endif
