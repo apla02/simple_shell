b8ac501567b6ff71cea7571674f1ad7ca9608730
apla02 <1873@holbertonschool.com>
2020-08-12 20:53:31 -0500

    change isatty

--- shell.c
+++ shell.c
@@ -2,21 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ * main - function to prompt the simple shell
+ * void: --
+ * Return: zero
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t n = 0; /*tamaño del buffer*/
-    ssize_t read;
-    int isat = isatty(STDIN_FILENO);
+	char *line = NULL;
+	size_t n = 0; /*tamaño del buffer*/
+	ssize_t read;
 
-    if (isat == 1)
-        write (1,"$ ", 2);
-    while ((read =getline(&line, &n, stdin )) != EOF)
-    {
-        _analize(line);
-        write (1,"$ ", 2);
-        free(line);
-        line = NULL, n = 0;
-    }
-    return (0);
+	if (isatty(STDIN_FILENO))
+		write(STDIN_FILENO, "$ ", 2);
+	while ((read = getline(&line, &n, stdin)) != EOF)
+	{
+		if (read == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+		_analize(line);
+		write(1, "$ ", 2);
+		free(line);
+		line = NULL, n = 0;
+	}
+	return (0);
 }
