3aa46a713932128876b21882a94b643359afa078
Juaco121 <juanfernandogranada@gmail.com>
2020-08-13 01:45:49 +0000

    New function for get a PATH and VARIABLE of enviroment

---
+++ _getenv.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *_getenv - looks at environment variable and returns a pointer to string
+ *@name: the name of the environmental variable
+ *
+ *Return: pointer to the environmental variable
+ */
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	char **env = environ;
+	int i = 0;
+	int j = 0;
+
+	if (name == NULL || env == NULL || *env == NULL)
+		return (NULL);
+	while (env[i] != NULL)
+	{
+		while (env[i][j] == name[j])
+			j++;
+		if (env[i][j] == '=')
+		{
+			j++;
+			return (&(env[i][j]));
+		}
+		i++;
+		j = 0;
+	}
+	write(STDOUT_FILENO, "Not found in environment", 24);
+	return (NULL);
+}
