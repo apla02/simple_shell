92b0117e1b61e2af3d2a9d1c14304fbe7ba02333
apla02 <1873@holbertonschool.com>
2020-08-18 15:34:24 -0500

    remove the line of exit

--- error.c
+++ error.c
@@ -9,5 +9,4 @@ void error(char **argv, char *message)
 	write(STDERR_FILENO, argv[0], _strlen(argv[0]));
 	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, message, _strlen(message));
-	exit(127);
 }
