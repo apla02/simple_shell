90b3d757cecdbe85f3d571d5cc5a5e0918f3b315
apla02 <1873@holbertonschool.com>
2020-08-19 09:16:49 -0500

    insert comments

--- holberton.h
+++ holberton.h
@@ -6,17 +6,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+/*Functions to handle strings*/
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_str_concat(char *s1, char *s2);
 char *_strdup(char *str);
+/*Functions related the environment*/
 char *_which(char *s);
 char *_getenv(const char *name);
 extern char **environ;
-int check_exit(char *str);
 char **_analize(char *str);
 int _execute(char **argv);
 int counter_paths(char *string);
 int counter_words(char *string);
+/*built-in exit*/
+int check_exit(char *str);
+/*display the error message*/
 void error_message(char **argv, int n);
 #endif
