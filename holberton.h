beebbedece1fad496af73f4cbb92de4bd9a64771
apla02 <1873@holbertonschool.com>
2020-08-19 09:05:46 -0500

    header file optimized

--- holberton.h
+++ holberton.h
@@ -1,11 +1,10 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
 #include <sys/stat.h>
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
@@ -13,11 +12,11 @@ char *_str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *_which(char *s);
 char *_getenv(const char *name);
+extern char **environ;
 int check_exit(char *str);
 char **_analize(char *str);
 int _execute(char **argv);
-extern char **environ;
 int counter_paths(char *string);
 int counter_words(char *string);
-void error_message(char **argv, int count);
+void error_message(char **argv, int n);
 #endif
