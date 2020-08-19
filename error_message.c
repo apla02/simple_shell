683f7436ce8fe6e974b738aff17082fa47dc36bb
apla02 <1873@holbertonschool.com>
2020-08-19 08:54:41 -0500

    function for the error message

---
+++ error_message.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * _putchar - print a character
+ * @c:character
+ * Return: zero
+*/
+int _putchar(char c)
+{
+	return (write(STDERR_FILENO, &c, 1));
+}
+/**
+ * error_message -fn to display the error message
+ * @argv: pointer of arrays
+ * @n:counts the errors
+ * Return: none
+*/
+void error_message(char **argv, int n)
+{
+	int mul, numlength, copy;
+
+	write(STDERR_FILENO, "hsh", 3);
+	write(STDERR_FILENO, ": ", 2);
+
+	copy = n;
+	mul = 1;
+	numlength = 1;
+	while (copy >= 10)
+	{
+		copy /= 10;
+		mul *= 10;
+		++numlength;
+	}
+	while (numlength > 1)
+	{
+		if ((n / mul) < 10)
+			_putchar((n / mul + '0'));
+		else
+			_putchar((n / mul) % 10 + '0');
+		--numlength;
+		mul /= 10;
+	}
+	_putchar(n % 10 + '0');
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+	write(STDERR_FILENO, ": not found\n", 12);
+}
+
